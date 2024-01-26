52f7c9ac5e4ffac263c4181d9c17a2cc78ca14c9
nyamarinette <nyamarinette2020@gmail.com>
2024-01-26 23:13:53 +0200

    first commit

---
+++ src/Cat.java
@@ -0,0 +1,7 @@
+public class Cat
+{
+    public Cat()
+    {
+        System.out.println("C'est un chat : " + this);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+    public static void main(String[] args)
+    {
+        //Ici chaque a son emplacement avec les différents memoires, miaou est différent de miaou2.
+
+        Cat miaou = new Cat();
+        Cat miaou2 = new Cat();
+
+        }
+    }
